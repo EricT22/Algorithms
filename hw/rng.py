0265f3680b602acb054470550d00d532ea67f1a1
eric <phantomenace87@gmail.com>
2024-09-09 14:57:11 -0700

    implementation and test of lcg algorithm

--- hw/rng.py
+++ hw/rng.py
@@ -1,6 +1,26 @@
-class rng:
+class RNG:
     def __init__(self, seed) -> None:
-        pass
+        self.m = 103
+        self.a = 21
+        self.c = 0
+        self.x = seed
 
-    def randInt(upper_bound):
-        pass
+
+    def _next(self):
+        self.x = (self.a * self.x + self.c) % self.m
+
+        return self.x
+
+
+    # Returns a random number between [1, bound] using the LCG algorith
+    def nextInt(self, upper_bound):
+        return (self._next() % upper_bound) + 1
+    
+
+# testing to make sure it works
+# rng = RNG(1)
+# arr = []
+# for i in range(5000):
+#     arr.append(rng.nextInt(100))
+
+# print(arr)
