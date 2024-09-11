a7fad4132ace829e4ac3ec3902af45dd58a25b13
eric <phantomenace87@gmail.com>
2024-09-11 14:04:40 -0700

    comments for heap permute

--- inclass/heapPermutation.py
+++ inclass/heapPermutation.py
@@ -4,11 +4,15 @@ N = len(test)
 
 def heapPermute(n):
     if n == 0:
+        # direct solution is to print results, which will be one permutation
         print(test)
     else:
         for i in range(0, n, 1):
+            # subdivision is reducing n by 1
+            # recurrence is n recursive calls
             heapPermute(n - 1)
             
+            # recombination is swapping values
             if n % 2 == 1:
                 temp = test[0]
                 test[0] = test[n - 1]
