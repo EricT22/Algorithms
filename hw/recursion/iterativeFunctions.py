0f35f5548f0f326db48852df9aeaa8b689579009
eric <phantomenace87@gmail.com>
2024-09-16 17:23:56 -0700

    finished iterative functions and tested them for functionality

--- hw/recursion/iterativeFunctions.py
+++ hw/recursion/iterativeFunctions.py
@@ -1,26 +1,72 @@
+import math
 from abstractFunctions import AbstractFuncts
 
 class IterativeFuncts(AbstractFuncts):
     @staticmethod
     def exponentiation(a, b):
-        pass
+        pow = 1
+        if b > 0:
+            for i in range(b):
+                pow *= a
+        elif b < 0:
+            for i in range(abs(b)):
+                pow *= 1 / a
+        
+        return pow
 
 
     @staticmethod
-    def string_reversal(str):
-        pass
+    def string_reversal(string):
+        string = list(string)
+
+        for i in range(len(string) // 2):
+            temp = string[i]
+            string[i] = string[len(string) - 1 - i]
+            string[len(string) - 1 - i] = temp
+
+        return "".join(string)
 
 
     @staticmethod
     def fibonacci(n):
-        pass
+        nmin2 = 0
+        nmin1 = 1
 
+        if n == 0:
+            return nmin2
+        elif n == 1:
+            return nmin1
+        else:
+            for i in range(1, n, 1):
+                fib = nmin1 + nmin2
+                nmin2 = nmin1
+                nmin1 = fib
+            
+            return fib
 
+    # dont work
     @staticmethod
     def is_palindrome(word):
-        pass
+        for i in range(len(word) // 2):
+            if word[i] != word[len(word) - 1 - i]:
+                return False
+            
+        return True
 
 
     @staticmethod
     def gcd(x, y):
-        pass
+        x = abs(x)
+        y = abs(y)
+
+        if y > x:
+            working_arr = [y, x]
+        else:
+            working_arr = [x, y]
+
+        while working_arr[1] != 0:
+            temp = working_arr[0]
+            working_arr[0] = working_arr[1]
+            working_arr[1] = temp % working_arr[1]
+        
+        return working_arr[0]
--- hw/recursion/test.py
+++ hw/recursion/test.py
@@ -4,63 +4,104 @@ from iterativeFunctions import IterativeFuncts
 if __name__ == "__main__":
     # exponentiation
     a, b = 3, 2
+    print("Recursion:")
     print(RecursiveFuncts.exponentiation(a, b))
-
+    print("Iteration:")
+    print(IterativeFuncts.exponentiation(a, b))
 
     a, b = 3, -3
+    print("Recursion:")
     print(RecursiveFuncts.exponentiation(a, b))
-    
+    print("Iteration:")
+    print(IterativeFuncts.exponentiation(a, b))
 
     a, b = -3, 5
+    print("Recursion:")
     print(RecursiveFuncts.exponentiation(a, b))
+    print("Iteration:")
+    print(IterativeFuncts.exponentiation(a, b))
 
 
     print("-------------------------------------")
     # string reversal
     string = "Bob!"
+    print("Recursion:")
     print(RecursiveFuncts.string_reversal(string))
-
+    print("Iteration:")
+    print(IterativeFuncts.string_reversal(string))
 
     string = "palooza"
+    print("Recursion:")
     print(RecursiveFuncts.string_reversal(string))
+    print("Iteration:")
+    print(IterativeFuncts.string_reversal(string))
 
     string = "1racecar1"
+    print("Recursion:")
     print(RecursiveFuncts.string_reversal(string))
+    print("Iteration:")
+    print(IterativeFuncts.string_reversal(string))
+
 
 
     print("-------------------------------------")
     # fibonacci
     n = 3
+    print("Recursion:")
     print(RecursiveFuncts.fibonacci(n))
-
+    print("Iteration:")
+    print(IterativeFuncts.fibonacci(n))
 
     n = 8
+    print("Recursion:")
     print(RecursiveFuncts.fibonacci(n))
+    print("Iteration:")
+    print(IterativeFuncts.fibonacci(n))
 
     n = 1
+    print("Recursion:")
     print(RecursiveFuncts.fibonacci(n))
+    print("Iteration:")
+    print(IterativeFuncts.fibonacci(n))
 
 
     print("-------------------------------------")
     # palindrome
     string = "Bob"
+    print("Recursion:")
     print(f"{string} is a palindrome?: ", RecursiveFuncts.is_palindrome(string), sep="")
-
+    print("Iteration:")
+    print(f"{string} is a palindrome?: ", IterativeFuncts.is_palindrome(string), sep="")
 
     string = "palooza"
+    print("Recursion:")
     print(f"{string} is a palindrome?: ", RecursiveFuncts.is_palindrome(string), sep="")
+    print("Iteration:")
+    print(f"{string} is a palindrome?: ", IterativeFuncts.is_palindrome(string), sep="")
 
     string = "1racecar1"
+    print("Recursion:")
     print(f"{string} is a palindrome?: ", RecursiveFuncts.is_palindrome(string), sep="")
+    print("Iteration:")
+    print(f"{string} is a palindrome?: ", IterativeFuncts.is_palindrome(string), sep="")
 
 
     print("-------------------------------------")
     # Greatest Common Divisor
     x, y = 4, 2
+    print("Recursion:")
     print(f"GCD between {x} and {y}: ", RecursiveFuncts.gcd(x, y), sep="")
+    print("Iteration:")
+    print(f"GCD between {x} and {y}: ", IterativeFuncts.gcd(x, y), sep="")
 
     x, y = 1465, 1172
+    print("Recursion:")
     print(f"GCD between {x} and {y}: ", RecursiveFuncts.gcd(x, y), sep="")
+    print("Iteration:")
+    print(f"GCD between {x} and {y}: ", IterativeFuncts.gcd(x, y), sep="")
 
     x, y = 12, 48
+    print("Recursion:")
     print(f"GCD between {x} and {y}: ", RecursiveFuncts.gcd(x, y), sep="")
+    print("Iteration:")
+    print(f"GCD between {x} and {y}: ", IterativeFuncts.gcd(x, y), sep="")
